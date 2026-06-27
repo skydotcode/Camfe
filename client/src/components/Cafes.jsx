b504814bf535c6519ac7b0ed2a9d4b8fb85c280d
sky04-coder <yadavsachinkumar56@gmail.com>
2026-06-27 15:11:53 +0530

    fix lading

--- client/src/components/Cafes.jsx
+++ client/src/components/Cafes.jsx
@@ -24,6 +24,8 @@ export const Cafes = ({cafeListing}) => {
     fetchCafe();
   }, []);
 
+  const list = cafeListing?.length ? cafeListing : cafes;
+
 
   if(loading) return <Loading/> ;
   
@@ -31,7 +33,7 @@ export const Cafes = ({cafeListing}) => {
     <div>
       <p className='text-2xl mt-2'>Popular Cafes</p>
         <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 pt-4 mb-8'>
-          {(cafes?.length !==0) && (cafeListing!= 0) ? (cafeListing || cafes)?.map((cafe) => (
+          {list?.length ? list?.map((cafe) => (
             <Cafecard key={cafe._id} cafe={cafe} />
           )) : <p className='flex justify-center font-bold
                     text-xsm text-[#fe6a36]'>No Results :( </p>
