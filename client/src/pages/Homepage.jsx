5667dd305be514037c57209f6dcdf549263157d3
sky04-coder <yadavsachinkumar56@gmail.com>
2026-08-03 23:25:24 +0530

    render service restored

--- client/src/pages/Homepage.jsx
+++ client/src/pages/Homepage.jsx
@@ -13,11 +13,11 @@ export const Homepage = () => {
       <div className='lg:px-30 px-4 min-h-screen'>
         <Search/>
         <Categories/>
-        <div className='flex justify-center items-center flex-col'>
+        {/* <div className='flex justify-center items-center flex-col'>
           <img className='m-4 lg:h-160 rounded-2xl shadow-md' src={`https://res.cloudinary.com/${import.meta.env.VITE_CLOUDINARY_CLOUD_NAME}/image/upload/v1784630810/ezvzcckp59muhipo7ej2.png`} />
           <p className='text-3xl text-[#fe6a37] font-bold'>Our Backend Server is down :(</p>
           <p>Will be back soon...</p>
-        </div>
+        </div> */}
         
         <Cafes />
         {/* <Fooditems/> */}
