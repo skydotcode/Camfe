0545ceec6652aa3caf3788dd7bdb4e6bb0c1ecba
sky04-coder <yadavsachinkumar56@gmail.com>
2026-08-04 00:06:41 +0530

    quick section

--- client/src/pages/Homepage.jsx
+++ client/src/pages/Homepage.jsx
@@ -4,6 +4,7 @@ import { Search } from '../components/Search';
 import { Categories } from '../components/Categories';
 import { Cafes } from '../components/Cafes';
 import { Footer } from '../components/Footer';
+import { QuickCravings } from '#src/components/QuickCravings.jsx';
 
 
 export const Homepage = () => {
@@ -13,6 +14,7 @@ export const Homepage = () => {
       <div className='lg:px-30 px-4 min-h-screen'>
         <Search/>
         <Categories/>
+        <QuickCravings/>
         {/* <div className='flex justify-center items-center flex-col'>
           <img className='m-4 lg:h-160 rounded-2xl shadow-md' src={`https://res.cloudinary.com/${import.meta.env.VITE_CLOUDINARY_CLOUD_NAME}/image/upload/v1784630810/ezvzcckp59muhipo7ej2.png`} />
           <p className='text-3xl text-[#fe6a37] font-bold'>Our Backend Server is down :(</p>
